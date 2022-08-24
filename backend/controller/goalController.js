e59e939f0f6529968aee8600fa250cae49c7fdfa
ronksks <ron.yalensky@gmail.com>
2022-08-24 11:41:49 +0300

    added asyncHandler

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,36 +1,41 @@
+// asyncHandler(express-async-handler) is a
+// simple middleware for handling exceptions inside of async express routes and passing them to your express error handlers.
+
+const asyncHandler = require('express-async-handler');
+
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
-const getGoals = async(req, res) => {
+const getGoals = asyncHandler(async(req, res) => {
 
     res.status(200).json({ message: 'Get goals' });
-}
+})
 
 // @desc    Set goals
 // @route   POST /api/goals
 // @access  Private
-const setGoal =  async(req, res) => {
+const setGoal =  asyncHandler(async(req, res) => {
     if (!req.body.text) {
         res.status(400);
         // uses the new error handler
         throw new Error('Please add a text field');
     }
     res.status(200).json({ message: 'Set goal' });
-}
+})
 
 // @desc    Update goal
 // @route   PUT /api/goals:id
 // @access  Private
-const updateGoal =  async(req, res) => {
+const updateGoal =  asyncHandler(async(req, res) => {
     res.status(200).json({ message: `Update goal ${req.params.id}` });
-}
+})
 
 // @desc    Delete goal
 // @route   DELETE /api/goals:id
 // @access  Private
-const deleteGoal =  async(req, res) => {
+const deleteGoal =  asyncHandler(async(req, res) => {
     res.status(200).json({ message: `Delete goal ${req.params.id}` });
-}
+})
 
 module.exports = {
     getGoals, setGoal, updateGoal, deleteGoal
