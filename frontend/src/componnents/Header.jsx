fdec143b9ced699b1e1b9418c6e738513554cdd7
ronksks <ron.yalensky@gmail.com>
2022-10-03 16:37:42 +0300

    added logout btn, fun, and clearing from local storage

--- frontend/src/componnents/Header.jsx
+++ frontend/src/componnents/Header.jsx
@@ -1,24 +1,44 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
 
 // Header page with Login, Register react icons including links to the designated pages that assinged Routes from the app.js
 function Header() {
+  const navigate = useNavigate();
+  const dispach = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+  const onLogout = () => {
+    dispach(logout());
+    dispach(reset());
+    navigate("/");
+  };
   return (
     <header className="header">
       <div className="logo">
         <Link to="/">GoalSetter</Link>
       </div>
       <ul>
-        <li>
-          <Link to="/login">
-            <FaSignInAlt /> Login
-          </Link>
-        </li>
-        <li>
-          <Link to="/register">
-            <FaUser /> Register
-          </Link>
-        </li>
+        {user ? (
+          <li>
+            <button className="btn" onClick={onLogout}>
+              <FaSignOutAlt /> Logout
+            </button>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </header>
   );
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -65,6 +65,9 @@ export const authSlice = createSlice({
         state.isError = true;
         state.message = action.payload;
         state.user = null;
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null;
       });
   },
 });
