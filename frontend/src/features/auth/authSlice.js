7f376116d4b84e4e4cbbcb1f370c20d503c447ee
ronksks <ron.yalensky@gmail.com>
2022-10-03 15:36:34 +0300

    added authService to handle the registrations

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
 
 // Get user from localStorage
 const user = JSON.parse(localStorage.getItem("user"));
@@ -11,11 +12,30 @@ const initialState = {
   message: "",
 };
 
+// Register user
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+        //return the message as the paylode trough the thunkAPI with the rejectWithValue func
+        return thunkAPI.rejectWithValue(message)
+    }
+  }
+);
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
   // regular non async functions
-  // ** after the form is dispached the followong values would reset ** 
+  // ** after the form is dispached the followong values would reset **
   reducers: {
     reset: (state) => {
       state.isLoading = false;
@@ -29,5 +49,5 @@ export const authSlice = createSlice({
 });
 
 // export the reset function as a component
-export const {reset} = authSlice.actions
-export default authSlice.reducer
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;
