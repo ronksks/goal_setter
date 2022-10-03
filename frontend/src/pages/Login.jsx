1aa790a1135543f4b38bd19001946753c2020c37
ronksks <ron.yalensky@gmail.com>
2022-10-03 14:19:56 +0300

    added a form to register and login pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,69 @@
+import { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
 
 function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  //    deconstruct the formData
+  const { email, password } = formData;
+
+  //    onChange we set the form data in a way each input would be the key, and th value would be what the user types in the same input
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>Login</div>
-  )
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please sign into your account</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+         
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter password"
+              onChange={onChange}
+            />
+          </div>
+         
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
-export default Login
+export default Login;
