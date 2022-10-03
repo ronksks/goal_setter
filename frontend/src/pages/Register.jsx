ddebc686876184096c0674e2bdfc3ef46bfc176d
ronksks <ron.yalensky@gmail.com>
2022-10-03 16:09:05 +0300

    added spinner component when register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { FaUser } from "react-icons/fa";
 import { register, reset } from "../features/auth/authSlice";
+import Spinner from '../componnents/Spinner'
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -56,6 +57,9 @@ function Register() {
       dispatch(register(userData));
     }
   };
+  if(isLoading){
+    return <Spinner />
+  }
 
   return (
     <>
