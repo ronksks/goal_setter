d237f9f2fecc5842680a52586b9a765e87cd8df7
ronksks <ron.yalensky@gmail.com>
2022-10-03 16:47:06 +0300

    added login funcunallity

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from "react";
 import { FaSignInAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { login, reset } from "../features/auth/authSlice";
+import Spinner from "../componnents/Spinner";
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -8,6 +13,25 @@ function Login() {
   });
   //    deconstruct the formData
   const { email, password } = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  // if one the the dependency array changes the useEffect called
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate("/");
+    }
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
 
   //    onChange we set the form data in a way each input would be the key, and th value would be what the user types in the same input
   const onChange = (e) => {
@@ -19,7 +43,15 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    const userData = {
+      email,
+      password,
+    };
+    dispatch(login(userData));
   };
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
@@ -31,7 +63,6 @@ function Login() {
       </section>
       <section className="form">
         <form onSubmit={onSubmit}>
-         
           <div className="form-group">
             <input
               type="email"
@@ -54,7 +85,7 @@ function Login() {
               onChange={onChange}
             />
           </div>
-         
+
           <div className="form-group">
             <button type="submit" className="btn btn-block">
               Submit
